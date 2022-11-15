7289d3d4580625ae237bc55c900c7a4d99998e53
Bobby Rong <rjl931189261@126.com>
2022-11-15 12:27:20 +0800

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs = inputs@{ self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    in
+    {
+      nixosConfigurations.vm = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ({ pkgs, ... }: {
+            imports = [ "${inputs.nixpkgs}/nixos/modules/virtualisation/qemu-vm.nix" ];
+
+            environment.systemPackages = with pkgs; [
+              ashpd-demo
+              d-spy
+              gdb
+              binutils
+              file
+              glib
+              htop
+              less
+            ];
+
+            services.xserver = {
+              enable = true;
+              desktopManager.gnome.flashback.enableMetacity = true;
+            };
+
+            users.users.test = {
+              isNormalUser = true;
+              uid = 1000;
+              extraGroups = [ "wheel" "networkmanager" ];
+              password = "test";
+            };
+            virtualisation = {
+              qemu.options = [ "-device intel-hda -device hda-duplex" ];
+              memorySize = 2048;
+              diskSize = 8192;
+            };
+
+            system.stateVersion = "22.11";
+          })
+        ];
+      };
+
+      packages.x86_64-linux.default = self.nixosConfigurations.vm.config.system.build.vm;
+      apps.x86_64-linux.default = {
+        type = "app";
+        program = "${self.packages.x86_64-linux.default}/bin/run-nixos-vm";
+      };
+    };
+}
